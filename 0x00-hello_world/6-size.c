0b2b34e9e317b81d5720405371ddc4bb3994e75a
FestusAjibola2100 <festusajibola8@gmail.com>
2022-10-19 01:51:46 -0700

    enter

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,11 @@
 
 int main(void)
 {
-	printf("size of a char: %ld byte(s)\n", sizeof(char));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("size of float: %ld byte(s)\n", sizeof(float));
+	printf("size of a char: %ld byte(s)\n", 
+			sizeof(char));
+	printf("size of a long long int: %ld byte(s)\n", 
+			sizeof(long long int));
+	printf("size of float: %ld byte(s)\n",
+			sizeof(float));
 	return (0);
 }
