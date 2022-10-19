c82946b42e4a12120a4d9cac7eeec450bb0a70d3
FestusAjibola2100 <festusajibola8@gmail.com>
2022-10-19 01:46:20 -0700

    sizes of data

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** main-is the entry point for the program
+ *
+ * Return: zero if no error, non-zero value if errors
+ */
+
+
+in main(void)
+{
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("size of float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
