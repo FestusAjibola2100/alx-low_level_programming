f7e86b8a38e7eb08d2ff2226be0b17b614410c67
FestusAjibola2100 <festusajibola8@gmail.com>
2022-10-19 01:23:21 -0700

    multi lingual

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 
 /**
  * main-serves as the entry point for the program
+ *
  * Return: By default returns zero if no errors otherwise returns non-zero value
  */
 
 int main(void)
 {
 	puts("\" Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
